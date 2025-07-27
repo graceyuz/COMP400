2be6da910a8880e53bdf32df9a6e88cae65adcc9
graceyuz <yuzwingrace@gmail.com>
2025-07-27 16:58:52 -0400

    Update spreadsheet-to-VADER.py
    Docsting and import statements

--- spreadsheet-to-VADER.py
+++ spreadsheet-to-VADER.py
@@ -1,3 +1,20 @@
 # Part 2:
 # Comments from the spreadsheet -> VADER evalutation -> Balancing with likes
 
+import csv
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+def vader_analysis (comment):
+    """
+    VADER compound evaluation of the given comment
+
+    Args: 
+        comment: The comment to be evaluated
+    
+    Returns:
+        VADER's int compound analysis of the comment
+    """
+
+    return None

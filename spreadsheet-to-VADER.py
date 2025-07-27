815cc8f5772d410b15deaa131014bbda1e49aaa1
graceyuz <yuzwingrace@gmail.com>
2025-07-27 17:31:31 -0400

    Update spreadsheet-to-VADER.py
    Output changed

--- spreadsheet-to-VADER.py
+++ spreadsheet-to-VADER.py
@@ -33,11 +33,12 @@ pos_comments = 0
 neg_comments = 0
 neu_comments = 0
 total_comments = 0
+unweighted_comments = 0
 total_sentiment = 0
 
 with open(file_name, "r", encoding="utf-8") as csv_file:
     # specify the delimeter is a comma
-    reader = csv.reader(csv_file, delimeter=",")
+    reader = csv.reader(csv_file, delimiter=",")
 
     # for each row, send the comment to VADER and weight with likes
     for row in reader:
@@ -45,7 +46,7 @@ with open(file_name, "r", encoding="utf-8") as csv_file:
         sentiment = vader_analysis(row[1])
 
         # check like count
-        like_count = row[0]
+        like_count = int(row[0])
 
         if like_count > 0:
             # counting sentiment of all likes
@@ -54,19 +55,21 @@ with open(file_name, "r", encoding="utf-8") as csv_file:
         # track comment sentiment totals, including likes on all comments
         # if positive
         if sentiment > 0.05:      
-            positive_comments += (1 + like_count)
+            pos_comments += (1 + like_count)
         # if negative
         elif sentiment < -0.05:     
-            negative_comments += (1 + like_count)
+            neg_comments += (1 + like_count)
         # if neutral
         else:                   
-            neutral_comments += (1 + like_count)
+            neu_comments += (1 + like_count)
 
         # total comment tracker
         total_comments += (1 + like_count)
+        unweighted_comments += 1
 
-print(positive_comments/total_comments*100, "% positive", "# pos comments: ", positive_comments, sep="")
-print(negative_comments/total_comments*100, "# neg comments: ", negative_comments, "% negative", sep="")
-print(neutral_comments/total_comments*100, "% neutral", "# neutral comments: ", neutral_comments, sep="")
+print(round(pos_comments/total_comments*100, 2), "% positive", ", number of positive comments: ", pos_comments, sep="")
+print(round(neg_comments/total_comments*100, 2), "% negative", ", number of negative comments: ", neg_comments, sep="")
+print(round(neu_comments/total_comments*100,2), "% neutral", ", number of neutral comments: ", neu_comments, sep="")
 print("total # comments: ", total_comments, sep="")
-print("average sentiment:", total_sentiment/total_comments)
+print(round(total_sentiment/total_comments, 4), "was the average sentiment")
+print("Number of comments before weight:", unweighted_comments)
